8522ef13b9119fbfe9245431727bc1c9f510fe51
sankalp patel <sankalppatel38@gmail.com>
2026-06-19 00:12:48 +0530

    done_routing_Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-
+import AllRoutes from './Routes/AllRoutes'
 function App() {
-
   return (
     <>
-    
+      <h2>Machine Coding Shopping Cart</h2>
+      <AllRoutes />
     </>
   )
 }
